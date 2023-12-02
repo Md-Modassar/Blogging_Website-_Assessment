7b359a0336acaab45bb6b5d089fd539396a8e8ba
Md-Modassar <mdmodassar622@gmail.com>
2023-12-02 13:16:00 +0530

    final push

--- src/Routes/PostRouter.js
+++ src/Routes/PostRouter.js
@@ -3,10 +3,10 @@ const router=express.Router()
 const {createpost, getallPost, getPostById, updatapost, deletePost} =require("../controller/PostContrpller")
 const {authentication,autherization}=require("../middelware/auth")
 
-router.post("/createpost",createpost);
-router.get("/getallpost",getallPost)
-router.get("/getpost/:postId",getPostById)
+router.post("/createpost",authentication,createpost);
+router.get("/getallpost",authentication,getallPost)
+router.get("/getpost/:postId",authentication,getPostById)
 router.put("/updatepost/:postId",authentication,autherization,updatapost)
-router.delete("/deletepost/:postId",deletePost)
+router.delete("/deletepost/:postId",authentication,autherization,deletePost)
 
 module.exports=router;
